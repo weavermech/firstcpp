8f1c908e488e3cb12a5d6f0e33a77e11f2744116
SH <sh01373@surrey.ac.uk>
2019-01-19 13:49:21 +0000

    1349

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+    int num1{0}, num2{0};
+
+    std::cout << "Enter first: ";
+    std::cin >> num1;
+
+    std::cout << "Enter second: ";
+    std::cin >> num2;
+
+    std::cout << "Sum is " << num1 + num2 << std::endl;
+
+}
